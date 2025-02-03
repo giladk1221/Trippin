9f3fbfb78247e43ec1fa65a276f4800067c7559c
giladkatz <giladk1221@gmail.com>
2025-02-03 09:05:25 +0200

    update offset

--- backend/fetch_airports.php
+++ backend/fetch_airports.php
@@ -1,6 +1,6 @@
 <?php
 // Aviationstack API URL and Access Key
-$apiUrl = "https://api.aviationstack.com/v1/airports?access_key=4819df886394506c1e23adf88be74a27&offset=12000";
+$apiUrl = "https://api.aviationstack.com/v1/airports?access_key=4819df886394506c1e23adf88be74a27&offset=11900";
 
 
 // Use file_get_contents to fetch the API response
